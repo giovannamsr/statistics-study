ccea47a662d979097a59d156471f8f86bf85b6d2
Giovanna Mendes de Sousa Rodrigues <giovanna.mendes.pro@gmail.com>
2024-01-08 14:27:21 -0300

    added cross-validation in knn_MSE.R

--- knn_MSE.R
+++ knn_MSE.R
@@ -44,7 +44,7 @@ for (i in 1:nrow(results)){
 results %>%
   arrange(mse)
 
-x_pred <- seq(8, 18, 0.1)
+x_pred <- seq(8, 18, 0.101)
 
 tibble(ajuste = x_pred,
        y_fit = knn.reg(train = data$x, 
@@ -57,4 +57,46 @@ tibble(ajuste = x_pred,
 
 # Cross Validation --------------------------------------------------------
 
+n_lots <- 10
+n_obs_lot <- n_obs/n_lots
 
+mse_aux <- vector("numeric", n_lots)
+
+lots <- sample(rep(1:n_lots, each = n_obs_lot))
+
+results2 <- tibble(df = 2:(n_obs - n_obs_lot),
+                   mse = NA_real_)
+
+for (i in 1:nrow(results2)) {
+  for (j in 1:n_lots) {
+    
+    # calculates the MSE of lot j and stores it in a vector
+    fit <- knn.reg(train = data$x, 
+                   test = matrix(validation$x), 
+                   y =  data$y, 
+                   k = i)
+    mse_aux[j] <-  mean((validation$y - fit$pred)^2)
+  }
+  
+  results2$mse[i] <- mean(mse_aux)
+  #store the average of the vector with the MSE in results2$mse[i]
+}
+
+results2 %>%
+  arrange(mse)
+
+results2 %>%
+  ggplot(aes(df, mse)) +
+  geom_point() +
+  geom_line()
+
+tibble(ajuste = x_pred,
+       y_fit = knn.reg(train = data$x, 
+                       test = matrix(validation$x), 
+                       y =  data$y, 
+                       k = 6)$pred) %>% 
+  ggplot(aes(ajuste, y_fit)) + 
+  geom_point(data = data, aes(x, y)) +
+  geom_step(color = "red")
+
+#With cross validation, we realized that the best average MSE is with k = 6
